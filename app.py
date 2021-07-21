c7e8060c7cf5e1e2ad8c1ab36b754f43e2308528
Nauber <naubergois@gmail.com>
2021-07-21 15:45:39 +0100

    CORS on service

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 
 from flask import Flask, render_template
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+CORS(app)
+
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
